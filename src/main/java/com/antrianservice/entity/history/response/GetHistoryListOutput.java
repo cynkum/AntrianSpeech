34c784c05da553c842dbc67d32abfe71af394fb9
cynkum <cynthia.kumalasari@si.ukdw.ac.id>
2021-01-14 08:49:24 +0700

    add GetHistoryListOutput

---
+++ src/main/java/com/antrianservice/entity/history/response/GetHistoryListOutput.java
@@ -0,0 +1,34 @@
+package com.antrianservice.entity.history.response;
+
+import com.antrianservice.response.ErrorSchema;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class GetHistoryListOutput {
+    private GetHistoryListResponse outputSchema;
+    private ErrorSchema errorSchema;
+
+    @Data
+    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+    public static class HistoryView{
+        private int idHistory;
+        private String nip;
+        private String idKategori;
+        private Date tanggal;
+        private Date start;
+        private Date finish;
+    }
+    @Data
+    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+    public static class GetHistoryListResponse{
+        private List<HistoryView> historyList;
+    }
+
+}
+
